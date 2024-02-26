3c5c79681c637db8c40efb630c0cd1d42637cf41
YuliiaSylvestr <jul.radchenko190891@gmail.com>
2024-02-26 11:31:24 +0200

    fix statistics h2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import s from './Statistics.module.css';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>{title}</h2>
+      {title && <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
         {stats.map(item => (
